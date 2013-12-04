fbdcba418517084623ae3d278b36905e93183071
Daniel Craik <daniel.charles.craik@cern.ch>
2013-12-04 18:58:04 +0000

    Updated 2D chi-squared code to use adaptive binning. closes #6
    git-svn-id: svn+ssh://phab.hepforge.org/source/laurasvn/trunk@32 30227d83-a2f9-49e9-9e98-657c83b2c86a

--- examples/CalcChiSq.hh
+++ examples/CalcChiSq.hh
@@ -1,17 +1,16 @@
-#include "TFile.h"
-#include "TH2.h"
+#include "TH2Poly.h"
 #include "TString.h"
 
 #include <vector>
 
 /*
    A utility class to allow the calculation of the chisq of the fit to the Dalitz plot.
-   A text config file is provided that gives the histograms for the data and
+   A text config file is provided that gives the datasets for the data and
    toy MC generated from the fit results.  These can be in the traditional DP
    or the square DP.  A sample config file is provided.  The fields are:
-   <name of file containing data histogram> <name of data histogram>
-   <name of file containing toy MC histogram> <name of toy MC histogram>
-   <the minimum content for any bin> <the number of free parameter in the fit> <the scalefactor by which the toy MC bin content should be multiplied>
+   <name of file containing data histogram> <name of data tree> <name of the x axis variable in data> <name of the y axis variable in data>
+   <name of file containing toy MC histogram> <name of toy MC tree> <name of the x axis variable in toy MC> <name of the y axis variable in toy MC>
+   <the minimum content for any bin> <the number of free parameter in the fit> <the scalefactor by which the toy MC bin content should be multiplied> <minimum x value> <maximum x value> <minimum y value> <maximum y value>
 */
 
 class CalcChiSq {
@@ -27,34 +26,22 @@ public:
 private:
 
 	void initialiseHistos();
-	void findLowestBin(Int_t& xBin, Int_t& yBin);
-	void findNeighbours(Int_t xBin, Int_t yBin);
-	void assignZeroBins();
+	void pickBinning(Double_t* xs, Double_t* ys, Int_t nEntries, std::vector<Int_t>& divisions);
+	void getHisto(Double_t xMin, Double_t xMax, Double_t yMin, Double_t yMax, Double_t* xs, Double_t* ys, Int_t nEntries, std::vector<Int_t>& divisions, UInt_t iter=0);
 
 	void calculateChiSq();
-
-	Bool_t findGroup(Int_t iBin, Int_t jBin);
-	Int_t findNearestGroup(Int_t iBin, Int_t jBin);
-
-	void setCheckBin(Int_t iBin, Int_t jBin, Int_t checkInt);
-	Int_t getCheckBinValue(Int_t iBin, Int_t jBin);
-	Float_t getHistBinValue(Int_t iBin, Int_t jBin);
-	Float_t getHist1BinValue(Int_t iBin, Int_t jBin);
-	Float_t getHist2BinValue(Int_t iBin, Int_t jBin);
-
-	Bool_t checkBin(Int_t iBin, Int_t jBin);
+	void makePlots();
 
 	TString inputFileName_;
-	TString fileName1_, fileName2_, histoName1_, histoName2_;
+	TString fileName1_, fileName2_, treeName1_, treeName2_, xName1_, xName2_, yName1_, yName2_;
 	Float_t minContent_;
 
-	TH2F *histo1_, *histo2_, *theHisto_;
-	TH2I *checkHisto_;
-	TH2F *chiSqHisto_;
-	TH2F *chiSqSignedHisto_;
+	TH2Poly *theHisto_, *histo1_, *histo2_,;
+	TH2Poly *pullHisto_;
+	TH2Poly *chiSqHisto_;
+	TH2Poly *chiSqSignedHisto_;
 
-	Int_t checkInt_, nCheckedBins_, nBinsX_, nBinsY_, nMaxBin_, nTotBins_;
-	Float_t xMin_, xMax_, yMin_, yMax_, maxContent_, groupWeight_, epsilon_;
+	Float_t xMin_, xMax_, yMin_, yMax_;
 	Int_t nParams_;
 	Float_t scaleFactor_;
 	Bool_t verbose_;

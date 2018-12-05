c3cb0694b55cb151c59fd1b1c759e8e06c319ce1
Thomas Latham <T.Latham@warwick.ac.uk>
2018-12-05 17:03:49 +0000

    Move sources and headers for the utilities library into the main library - makes examples dir executable only
    git-svn-id: svn+ssh://phab.hepforge.org/source/laurasvn/trunk@562 30227d83-a2f9-49e9-9e98-657c83b2c86a

--- examples/MergeDataFiles.cc
+++ examples/MergeDataFiles.cc
@@ -1,6 +1,6 @@
 
 /*
-Copyright 2008 University of Warwick
+Copyright 2013 University of Warwick
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,282 +22,34 @@ Paul Harrison
 Thomas Latham
 */
 
-#include "MergeDataFiles.hh"
-
 #include <iostream>
-#include <map>
 
-#include "TLeaf.h"
-#include "TObjArray.h"
-#include "TSystem.h"
+#include "TString.h"
 
-MergeDataFiles::MergeDataFiles(const TString& fileName1, const TString& fileName2, const TString& treeName) :
-	fileName1_(fileName1),
-	fileName2_(fileName2),
-	treeName_(treeName),
-	inputFile1_(0),
-	inputFile2_(0),
-	inputTree1_(0),
-	inputTree2_(0),
-	outputFile_(0),
-	outputTree_(0)
+#include "LauMergeDataFiles.hh"
+
+int main(const int argc, const char** argv)
 {
-}
-
-MergeDataFiles::~MergeDataFiles()
-{
-	if (inputFile1_ && inputFile1_->IsOpen()) {
-		inputFile1_->Close();
-	}
-	delete inputFile1_;
-
-	if (inputFile2_ && inputFile2_->IsOpen()) {
-		inputFile2_->Close();
-	}
-	delete inputFile2_;
-
-	if (outputFile_ && outputFile_->IsOpen()) {
-		outputFile_->Close();
-	}
-	delete outputFile_;
-}
-
-void MergeDataFiles::openInputFiles()
-{
-	// open the two input ROOT files
-	inputFile1_ = TFile::Open(fileName1_);
-	if (!inputFile1_ || inputFile1_->IsZombie()) {
-		std::cerr<<"Problem opening file: \""<<fileName1_<<"\", exiting..."<<std::endl;
-		gSystem->Exit(EXIT_FAILURE);
-	}
-	inputTree1_ = dynamic_cast<TTree*>( inputFile1_->Get(treeName_) );
-	if (!inputTree1_) {
-		std::cerr<<"Problem getting tree called "<<treeName_<<"from file: \""<<fileName1_<<"\", exiting..."<<std::endl;
-		gSystem->Exit(EXIT_FAILURE);
+	if (argc < 3) {
+		std::cerr<<"Usage: "<<argv[0]<<" <inputFileName1> <inputFileName2> [treeName] [outputFileName]"<<std::endl;
+		return EXIT_FAILURE;
 	}
 
-	inputFile2_ = TFile::Open(fileName2_);
-	if (!inputFile2_ || inputFile2_->IsZombie()) {
-		std::cerr<<"Problem opening file: \""<<fileName2_<<"\", exiting..."<<std::endl;
-		gSystem->Exit(EXIT_FAILURE);
-	}
-	inputTree2_ = dynamic_cast<TTree*>( inputFile2_->Get(treeName_) );
-	if (!inputTree2_) {
-		std::cerr<<"Problem getting tree called "<<treeName_<<"from file: \""<<fileName2_<<"\", exiting..."<<std::endl;
-		gSystem->Exit(EXIT_FAILURE);
-	}
-}
+	TString inputFileName1(argv[1]);
+	TString inputFileName2(argv[2]);
 
-void MergeDataFiles::setupInputTrees()
-{
-	TObjArray* leaves1 = inputTree1_->GetListOfLeaves();
-	TObjArray* leaves2 = inputTree2_->GetListOfLeaves();
-	Int_t nLeaves1 = leaves1->GetEntries();
-	Int_t nLeaves2 = leaves2->GetEntries();
-	if ( nLeaves1 != nLeaves2 ) {
-		std::cerr<<"Different number of leaves in the two input trees, not continuing."<<std::endl;
-		return;
+	TString treeName("fitTree");
+	if (argc > 3) {
+		treeName = argv[3];
+	}
+	TString outputFileName("test.root");
+	if (argc > 4) {
+		outputFileName = argv[4];
 	}
 
-	std::cout<<"Setting branches for input trees \""<<treeName_<<"\"..."<<std::endl;
-	inputTree1_->SetBranchAddress("iExpt",&iExpt_);
-	inputTree1_->SetBranchAddress("iEvtWithinExpt",&iEvtWithinExpt_);
-	inputTree2_->SetBranchAddress("iExpt",&iExpt_);
-	inputTree2_->SetBranchAddress("iEvtWithinExpt",&iEvtWithinExpt_);
+	MergeDataFiles myMerger(inputFileName1,inputFileName2,treeName);
+	myMerger.process(outputFileName);
 
-	for (Int_t iLeaf(0); iLeaf<nLeaves1; ++iLeaf) {
-
-		TLeaf * leaf = dynamic_cast<TLeaf*>((*leaves1)[iLeaf]);
-		TString type = leaf->GetTypeName();
-		TString name = leaf->GetName();
-		Int_t size = leaf->GetNdata();
-
-		if ((name == "iExpt") || (name == "iEvtWithinExpt") || (size != 1)) {
-			continue;
-		}
-
-		if ( type == "Double_t" ) {
-			std::pair<LeafDoubleMap::iterator,bool> result = doubleVars_.insert(std::make_pair(name,0.0));
-			LeafDoubleMap::iterator iter = result.first;
-			bool ok = result.second;
-			if (ok) {
-				inputTree1_->SetBranchAddress(name,&(iter->second));
-				inputTree2_->SetBranchAddress(name,&(iter->second));
-			}
-		} else if ( type == "Int_t" ) {
-			std::pair<LeafIntegerMap::iterator,bool> result = integerVars_.insert(std::make_pair(name,0));
-			LeafIntegerMap::iterator iter = result.first;
-			bool ok = result.second;
-			if (ok) {
-				inputTree1_->SetBranchAddress(name,&(iter->second));
-				inputTree2_->SetBranchAddress(name,&(iter->second));
-			}
-		}
-	}
-	std::cout<<"Set branch addresses for "<<doubleVars_.size()<<" Double_t branches.\n";
-	std::cout<<"Set branch addresses for "<<integerVars_.size()<<" Int_t branches.\n"<<std::endl;
-}
-
-void MergeDataFiles::setupOutputTree()
-{
-	std::cout<<"Creating branches for output tree \""<<outputTree_->GetName()<<"\"..."<<std::endl;
-
-	outputTree_->Branch("iExpt",&iExpt_,"iExpt/I");
-	outputTree_->Branch("iEvtWithinExpt",&iEvtWithinExpt_,"iEvtWithinExpt/I");
-
-	for (LeafDoubleMap::iterator iter = doubleVars_.begin(); iter != doubleVars_.end(); ++iter) {
-		TString name = iter->first;
-		Double_t * address = &(iter->second);
-		TString thirdBit = name;
-		thirdBit += "/D";
-
-		outputTree_->Branch(name,address,thirdBit);
-	}
-	for (LeafIntegerMap::iterator iter = integerVars_.begin(); iter != integerVars_.end(); ++iter) {
-		TString name = iter->first;
-		Int_t * address = &(iter->second);
-		TString thirdBit = name;
-		thirdBit += "/I";
-
-		outputTree_->Branch(name,address,thirdBit);
-	}
-	std::cout<<"Created "<<doubleVars_.size()+integerVars_.size()<<" branches.\n"<<std::endl;
-}
-
-void MergeDataFiles::process(const TString& fileName)
-{
-	this->openInputFiles();
-
-	this->setupInputTrees();
-
-	outputFile_ = TFile::Open(fileName,"recreate");
-	outputTree_ = new TTree(treeName_,"");
-	this->setupOutputTree();
-
-	// loop over the trees and combine the corresponding experiments
-	std::cout<<"Starting to combine the trees..."<<std::endl;
-
-	// Find the first and last entries for each experiment in each tree
-	this->findExperiments( inputTree1_, tree1Expts_ );
-	this->findExperiments( inputTree2_, tree2Expts_ );
-
-	// Check that the experiments in the two trees match
-	if ( !this->checkExperimentMaps() ) {
-		return;
-	}
-
-	// Loop through the experiments
-	for ( ExptsMap::const_iterator iter1 = tree1Expts_.begin(); iter1 != tree1Expts_.end(); ++iter1 ) {
-
-		// get the map element for tree2
-		Int_t expt = iter1->first;
-		ExptsMap::const_iterator iter2 = tree2Expts_.find( expt );
-
-		// determine the number of entries in tree1
-		Int_t nEntriesInTree1 = iter1->second.second - iter1->second.first + 1;
-
-		// read the entries from the trees, filling the output tree
-		this->readExperiment( inputTree1_, iter1, 0 );
-		this->readExperiment( inputTree2_, iter2, nEntriesInTree1 );
-	}
-
-	// Write the output file
-	this->writeFile();
-}
-
-void MergeDataFiles::findExperiments(TTree* tree, ExptsMap& exptsMap)
-{
-	const Int_t nEntries = tree->GetEntries();
-
-	// loop through the tree
-	for ( Int_t iEntry(0); iEntry<nEntries; ++iEntry ) {
-		// grab the entry
-		tree->GetEntry(iEntry);
-
-		// see if we already have an element in the map for the
-		// current experiment
-		ExptsMap::iterator iter = exptsMap.find(iExpt_);
-		if ( iter == exptsMap.end() ) {
-			// if not, we need to add an element that points to
-			// this entry in the tree as the start entry
-			exptsMap.insert( std::make_pair( iExpt_, std::make_pair( iEntry, -99 ) ) );
-
-			// also we need to complete the map element for the
-			// previous experiment with the previous tree entry
-			// as the last entry
-			ExptsMap::iterator previter = exptsMap.find(iExpt_-1);
-			if ( previter != exptsMap.end() ) {
-				previter->second.second = iEntry-1;
-			}
-		}
-	}
-	// need to complete the map element for the final experiment
-	exptsMap[iExpt_].second = nEntries-1;
-}
-
-Bool_t MergeDataFiles::checkExperimentMaps() const
-{
-	// first check that the two maps are the same size
-	UInt_t size1 = tree1Expts_.size();
-	UInt_t size2 = tree2Expts_.size();
-	if ( size1 != size2 ) {
-		std::cerr<<"ERROR in MergeDataFiles::checkExperimentMaps : Experiment maps are not the same size.\n";
-		std::cerr<<"                                             : Tree from "<<fileName1_<<" has "<<size1<<" experiments.";
-		std::cerr<<"                                             : Tree from "<<fileName2_<<" has "<<size2<<" experiments.";
-		return kFALSE;
-	}
-
-	for ( ExptsMap::const_iterator iter1 = tree1Expts_.begin(); iter1 != tree1Expts_.end(); ++iter1 ) {
-		Int_t expt = iter1->first;
-		ExptsMap::const_iterator iter2 = tree2Expts_.find( expt );
-		if ( iter2 == tree2Expts_.end() ) {
-			std::cerr<<"ERROR in MergeDataFiles::checkExperimentMaps : Cannot find experiment "<<expt<<" in tree from "<<fileName2_<<std::endl;
-			return kFALSE;
-		}
-	}
-
-	return kTRUE;
-}
-
-void MergeDataFiles::readExperiment(TTree* tree, const ExptsMap::const_iterator& expt, Int_t offset)
-{
-	// find the first and last entry for this experiment from the map element
-	const Int_t firstEntry = expt->second.first;
-	const Int_t lastEntry = expt->second.second;
-
-	// loop through all the entries
-	for ( Int_t iEntry(firstEntry); iEntry<=lastEntry; ++iEntry ) {
-		// get the entry from the tree
-		tree->GetEntry( iEntry );
-		// apply the offset to the "event within experiment" variable
-		iEvtWithinExpt_ += offset;
-		// fill the output tree
-		outputTree_->Fill();
-	}
-}
-
-void MergeDataFiles::writeFile()
-{
-	std::cout<<"Building experiment:event index"<<std::endl;
-	outputTree_->BuildIndex("iExpt","iEvtWithinExpt");
-
-	std::cout<<"Writing data to outputfile "<<outputFile_->GetName()<<std::endl;
-	outputTree_->SetDirectory(outputFile_);
-	outputFile_->Write();
-
-	// clean-up
-	outputFile_->Close();
-	delete outputFile_; outputFile_ = 0; outputTree_ = 0;
-
-	inputFile1_->Close();
-	delete inputFile1_; inputFile1_ = 0; inputTree1_ = 0;
-
-	inputFile2_->Close();
-	delete inputFile2_; inputFile2_ = 0; inputTree2_ = 0;
-
-	doubleVars_.clear();
-	integerVars_.clear();
-	tree1Expts_.clear();
-	tree2Expts_.clear();
+	return EXIT_SUCCESS;
 }
 
--- examples/ResultsExtractor.cc
+++ examples/ResultsExtractor.cc
@@ -1,6 +1,6 @@
 
 /*
-Copyright 2005 University of Warwick
+Copyright 2013 University of Warwick
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,274 +22,34 @@ Paul Harrison
 Thomas Latham
 */
 
-#include "ResultsExtractor.hh"
-
-#include <iostream>
-#include <fstream>
 #include <cstdlib>
-#include <vector>
-#include <map>
 
-#include "TChain.h"
-#include "TFile.h"
-#include "TH1.h"
-#include "TLeaf.h"
-#include "TObjArray.h"
-#include "TSystem.h"
+#include "TString.h"
 
-using std::cout;
-using std::cerr;
-using std::flush;
-using std::endl;
-using std::vector;
-using std::map;
-using std::pair;
-using std::ofstream;
+#include "LauResultsExtractor.hh"
 
-ResultsExtractor::ResultsExtractor(const TString& inputFileName, const TString& outputFileName, const TString& treeName) :
-	inputFileName_(inputFileName),
-	outputFileName_(outputFileName),
-	treeName_(treeName),
-	inputTree_(0),
-	outputFile_(0),
-	outputTree_(0),
-	nEntries_(0)
+int main(const int argc, const char** argv)
 {
+	Int_t nExpt(1);
+	TString inputFileName("input-list.txt");
+	TString outputFileName("output.root");
+	TString treeName("fitResults");
+
+	if (argc > 1) {
+		nExpt = atoi(argv[1]);
+	}
+	if (argc > 2) {
+		inputFileName = argv[2];
+	}
+	if (argc > 3) {
+		outputFileName = argv[3];
+	}
+	if (argc > 4) {
+		treeName = argv[4];
+	}
+
+	ResultsExtractor myExtractor(inputFileName,outputFileName,treeName);
+	myExtractor.process(nExpt);
+
+	return EXIT_SUCCESS;
 }
-
-ResultsExtractor::~ResultsExtractor()
-{
-	this->clearMaps();
-	delete inputTree_; inputTree_ = 0;
-	if (outputFile_ && outputFile_->IsOpen()) {
-		delete outputTree_; outputTree_ = 0;
-	}
-	delete outputFile_; outputFile_ = 0;
-}
-
-void ResultsExtractor::setupInputTree()
-{
-	TObjArray* leaves = inputTree_->GetListOfLeaves();
-	Int_t nLeaves = leaves->GetEntries();
-
-	cout<<"Setting branches for input tree \""<<inputTree_->GetName()<<"\" with "<<nEntries_<<" entries..."<<endl;
-	inputTree_->SetBranchAddress("iExpt",&iExpt_);
-	inputTree_->SetBranchAddress("fitStatus",&fitStatus_);
-	inputTree_->SetBranchAddress("NLL",&NLL_);
-
-	for (Int_t iLeaf(3); iLeaf<nLeaves; ++iLeaf) {
-
-		TLeaf * leaf = dynamic_cast<TLeaf*>((*leaves)[iLeaf]);
-		TString type = leaf->GetTypeName();
-		TString name = leaf->GetName();
-		Int_t size = leaf->GetNdata();
-
-		if ((type != "Double_t") || (size != 1)) {
-			continue;
-		}
-
-		std::pair<std::map<TString,Double_t>::iterator,bool> result = otherVars_.insert(std::make_pair(name,0.0));
-		std::map<TString,Double_t>::iterator iter = result.first;
-		bool ok = result.second;
-		if (ok) {
-			inputTree_->SetBranchAddress(name,&(iter->second));
-		}
-	}
-
-	cout<<"Set branch addresses for "<<otherVars_.size()+3<<" branches.\n"<<endl;
-}
-
-void ResultsExtractor::setupOutputTree(TTree * tree)
-{
-	cout<<"Creating branches for output tree \""<<tree->GetName()<<"\"..."<<endl;
-
-	tree->Branch("iExpt",&iExpt_,"iExpt/I");
-	tree->Branch("fitStatus",&fitStatus_,"fitStatus/I");
-	tree->Branch("NLL",&NLL_,"NLL/D");
-
-	for (std::map<TString,Double_t>::iterator iter = otherVars_.begin(); iter != otherVars_.end(); ++iter) {
-		TString name = iter->first;
-		Double_t * address = &(iter->second);
-		TString thirdBit = name;
-		thirdBit += "/D";
-
-		tree->Branch(name,address,thirdBit);
-	}
-	cout<<"Created "<<otherVars_.size()<<" branches.\n"<<endl;
-}
-
-void ResultsExtractor::setInputTreeBranchStatus(Bool_t status)
-{
-	inputTree_->SetBranchStatus("iExpt",kTRUE);
-	inputTree_->SetBranchStatus("fitStatus",kTRUE);
-	inputTree_->SetBranchStatus("NLL",kTRUE);
-
-	for (std::map<TString,Double_t>::iterator iter = otherVars_.begin(); iter != otherVars_.end(); ++iter) {
-		TString name = iter->first;
-		inputTree_->SetBranchStatus(name,status);
-	}
-}
-
-void ResultsExtractor::clearMaps()
-{
-	for (map<Int_t,TH1*>::iterator iter = nllHistos_.begin(); iter != nllHistos_.end(); ++iter) {
-		delete (iter->second);
-	}
-	bestNLL_.clear();
-	worstNLL_.clear();
-	allNLLs_.clear();
-	nllHistos_.clear();
-}
-
-void ResultsExtractor::process(Int_t numExpts)
-{
-	// open the text file
-	cout << "\n" << "Chaining...\n" << endl;
-	std::ifstream textFile(inputFileName_, std::ios::in);
-	if (!textFile.good()) {
-		cerr<<"Problem opening file: \""<<inputFileName_<<"\", exiting..."<<endl;
-		gSystem->Exit(EXIT_FAILURE);
-	}
-
-	if (inputTree_) { delete inputTree_;  inputTree_ = 0; }
-	inputTree_ = new TChain(treeName_);
-
-	// Read the text file and add each valid entry to the chain
-	TString inputFileName = "";
-	while(inputFileName.ReadLine(textFile) && (!inputFileName.IsNull())) {
-		if (inputFileName.EndsWith(".root") && !inputFileName.BeginsWith("#")) {
-			cout << inputFileName << endl;
-			inputTree_->Add(inputFileName);
-		}
-		else {
-			cout << inputFileName << "\t *** Skipped ***" << endl;
-		}
-	}
-
-	textFile.close();
-	cout << "\n" << "... finished.\n" << endl;
-
-	nEntries_ = inputTree_->GetEntries();
-	this->setupInputTree();
-
-	outputTree_ = new TTree(treeName_,"");
-	this->setupOutputTree(outputTree_);
-
-	// setup the map:
-	// for each experiment there is a pair object holding
-	// the best NLL and the tree entry for that NLL value
-	// each expt starts out with NLL = 0.0 and entry = -1
-	cout<<"Setting up the map..."<<flush;
-	this->clearMaps();
-	for (Int_t i(0); i<numExpts; ++i) {
-		bestNLL_.insert(std::make_pair(i, std::make_pair(0.0,-1)));
-		worstNLL_.insert(std::make_pair(i, std::make_pair(0.0,-1)));
-		allNLLs_.insert(std::make_pair(i, vector<Double_t>()));
-		allNLLs_[i].reserve(nEntries_);
-	}
-	cout<<" done.\n"<<endl;
-
-	// only read the 3 info branches
-	//this->setInputTreeBranchStatus(kFALSE);
-
-	// loop over the tree and store the best entries for each expt
-	cout<<"Starting to store best entry info..."<<endl;
-	for (Int_t j(0); j<nEntries_; ++j) {
-
-		if ((nEntries_<100) || (j%(nEntries_/100)==0)) {
-			cout<<"Examining entry "<<j<<endl;
-		}
-
-		inputTree_->GetEntry(j);
-
-		if ( (fitStatus_ == 3) && (NLL_ > -DBL_MAX/10.0) ) {
-			allNLLs_[iExpt_].push_back(NLL_);
-
-			Double_t curBestNLL = bestNLL_[iExpt_].first;
-			Int_t curBestEntry = bestNLL_[iExpt_].second;
-			if ((NLL_ < curBestNLL) || (curBestEntry == -1)) {
-				bestNLL_[iExpt_] = std::make_pair(NLL_,j);
-			}
-
-			Double_t curWorstNLL = worstNLL_[iExpt_].first;
-			Int_t curWorstEntry = worstNLL_[iExpt_].second;
-			if ((NLL_ > curWorstNLL) || (curWorstEntry == -1)) {
-				worstNLL_[iExpt_] = std::make_pair(NLL_,j);
-			}
-		}
-
-	}
-	cout<<"Finished storing best entry info.\n"<<endl;
-
-	cout<<"Creating NLL histograms..."<<flush;
-	TH1* histo(0);
-	for (Int_t i(0); i<numExpts; ++i) {
-		Double_t min = bestNLL_[i].first;
-		Double_t max = worstNLL_[i].first;
-		Double_t range = max - min;
-		if (range < 1e-3) {
-			min -= 0.005;
-			max += 0.005;
-		} else {
-			min -= range*0.2;
-			max += range*0.2;
-		}
-		TString name("expt");
-		name += i;
-		name += "NLL";
-		histo = new TH1F(name,"",100,min,max);
-		for (vector<Double_t>::const_iterator iter = allNLLs_[i].begin(); iter != allNLLs_[i].end(); ++iter) {
-			histo->Fill(*iter);
-		}
-		nllHistos_.insert(std::make_pair(i, histo));
-	}
-	cout<<" done.\n"<<endl;
-
-	// now need to read all branches
-	//this->setInputTreeBranchStatus(kTRUE);
-
-	std::ofstream fout("best-fit.txt");
-
-	// loop over the experiments, grab the best entry and store it
-	cout<<"Starting to retrieve best entries and fill output tree."<<endl;
-	for (Int_t i(0); i<numExpts; ++i) {
-		Int_t bestEntry = bestNLL_[i].second;
-		if (bestEntry != -1) {
-			inputTree_->GetEntry(bestEntry);
-			outputTree_->Fill();
-		}
-		if ((numExpts<100) || (i%(numExpts/100)==0)) {
-			cout<<"Writing out experiment "<<i<<endl;
-		}
-		TString bestFit(inputTree_->GetCurrentFile()->GetName());
-		bestFit.Remove(0,3);
-		Int_t index = bestFit.Index("_");
-		if ( index < 1 ) {
-			index = bestFit.Index(".");
-		}
-		bestFit.Remove(index);
-		fout<<"Experiment "<<i<<" BestFit "<<bestFit<<endl;
-	}
-	cout<<"Finished filling best entries in output tree.\n"<<endl;
-	fout.close();
-
-	cout<<"Writing output file."<<endl;
-
-	this->writeFile();
-}
-
-void ResultsExtractor::writeFile()
-{
-	if (!outputFile_) {
-		outputFile_ = new TFile(outputFileName_,"recreate");
-	}
-	for (map<Int_t,TH1*>::iterator iter = nllHistos_.begin(); iter != nllHistos_.end(); ++iter) {
-		(iter->second)->SetDirectory(outputFile_);
-	}
-	outputTree_->SetDirectory(outputFile_);
-	outputFile_->Write();
-	outputFile_->Close();
-	delete outputFile_; outputFile_ = 0;
-	nllHistos_.clear();
-}
-

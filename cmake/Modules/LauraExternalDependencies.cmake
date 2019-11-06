cd65b0a4227d9336c0371bc5e8325ad475735b14
Thomas Latham <T.Latham@warwick.ac.uk>
2019-11-06 18:35:37 +0000

    First draft of CMake build system
    Needs testing on various platforms
    Need to add (optional) build of doxygen docs
    Need to test that the exporting of the library target works

---
+++ cmake/Modules/LauraExternalDependencies.cmake
@@ -0,0 +1,24 @@
+# Laura++ needs ROOT, we don't yet specify a version...
+# We may depend on other things in the future, e.g. JSON or YAML for easier configuration of particle properties etc.
+
+if(DEFINED ENV{ROOTSYS})
+    list(APPEND CMAKE_PREFIX_PATH $ENV{ROOTSYS})
+endif()
+
+if(LAURA_BUILD_ROOFIT_SLAVE)
+	find_package(ROOT REQUIRED COMPONENTS EG RooFitCore RooFit)
+else()
+	find_package(ROOT REQUIRED COMPONENTS EG)
+endif()
+
+#message(STATUS "ROOT include directories: ${ROOT_INCLUDE_DIRS}")
+#message(STATUS "ROOT libraries: ${ROOT_LIBRARIES}")
+#message(STATUS "ROOT definitions: ${ROOT_DEFINITIONS}")
+#message(STATUS "ROOT CXX flags: ${ROOT_CXX_FLAGS}")
+#message(STATUS "ROOT CC flags: ${ROOT_CC_FLAGS}")
+#message(STATUS "ROOT use file: ${ROOT_USE_FILE}")
+# Don't want to do this because it uses old-style CMake
+#include(${ROOT_USE_FILE})
+
+include(${ROOT_DIR}/modules/RootNewMacros.cmake)
+

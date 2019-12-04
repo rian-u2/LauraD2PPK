b9ad46aed16527caf680c9ece895777ea614133e
Thomas Latham <T.Latham@warwick.ac.uk>
2019-12-04 12:28:32 +0000

    Specify ROOT minimum version 6.14

--- cmake/Modules/LauraExternalDependencies.cmake
+++ cmake/Modules/LauraExternalDependencies.cmake
@@ -1,4 +1,4 @@
-# Laura++ needs ROOT, we don't yet specify a version...
+# Laura++ needs ROOT, we just require that it is a version that exports CMake targets properly
 # We may depend on other things in the future, e.g. JSON or YAML for easier configuration of particle properties etc.
 
 if(DEFINED ENV{ROOTSYS})
@@ -6,9 +6,9 @@ if(DEFINED ENV{ROOTSYS})
 endif()
 
 if(LAURA_BUILD_ROOFIT_SLAVE)
-	find_package(ROOT REQUIRED COMPONENTS EG RooFitCore RooFit)
+	find_package(ROOT 6.14 REQUIRED COMPONENTS EG RooFitCore RooFit)
 else()
-	find_package(ROOT REQUIRED COMPONENTS EG)
+	find_package(ROOT 6.14 REQUIRED COMPONENTS EG)
 endif()
 
 #message(STATUS "ROOT include directories: ${ROOT_INCLUDE_DIRS}")
